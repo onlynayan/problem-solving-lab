e014d17020b819f8eaacbf1a0b2f7250d6184647
onlynayan <nayan271997@gmail.com>
2026-05-03 10:37:38 +0600

    Add Leetcode Reverse Polish Notation

---
+++ medium/Leetcode-Reverse_Polish_Notation.py
@@ -0,0 +1,37 @@
+# Problem: Reverse Polish Notation
+# Platform: LeetCode
+# Difficulty: Medium
+# Link: https://leetcode.com/problems/evaluate-reverse-polish-notation/
+
+from typing import List
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        operators = ["+","-","*","/"]
+        stack = []
+        if (len(tokens)==1):
+            result = int(tokens[-1])
+        else:
+            for i in tokens:
+                if i not in operators:
+                    stack.append(i)
+                if i in operators:
+                    a = int(stack[-1])
+                    stack.pop()
+                    b = int(stack[-1])
+                    stack.pop()
+                    if (i == "+"):
+                        stack.append(b+a)
+                    if (i == "-"):
+                        stack.append(b-a)
+                    if (i == "*"):
+                        stack.append(b*a)
+                    if (i == "/"):
+                        stack.append(int(b/a))
+
+            result = stack[-1]
+        return result
+    
+tokens = ["2","1","+","3","*"]
+sol = Solution()
+print(sol.evalRPN(tokens))
